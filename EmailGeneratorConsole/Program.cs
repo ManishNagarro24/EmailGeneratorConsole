c67356421075feab5e7f4eae47e7c23bc0b3357e
Manish Kumar <Manish.Kumar@blackbaud.me>
2023-05-24 18:08:21 +0530

    Add project files.

---
+++ EmailGeneratorConsole/Program.cs
@@ -0,0 +1,18 @@
+﻿using Azure.AI.OpenAI;
+using Azure;
+
+
+OpenAIClient client = new OpenAIClient(
+        new Uri("https://emailgeneratordemo.openai.azure.com/"),
+        new AzureKeyCredential("4461d4ebc79a45bca18557145962a4f3"));
+
+string deploymentName = "EmailGeneratorDemo01";
+string prompt = "Draft an email requesting fund for stray dogs.";
+Console.Write($"Input: {prompt}");
+
+Response<Completions> completionsResponse = client.GetCompletions(deploymentName, prompt);
+string completion = completionsResponse.Value.Choices[0].Text;
+
+Console.WriteLine(completion);
+
+Console.ReadLine();
